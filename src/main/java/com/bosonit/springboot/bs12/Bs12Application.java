d2334a2ecea2c9602e1b5eeebca20dcbce6e93de
Samuel Heredia <samuel.heredia@bosonit.com>
2021-10-20 11:13:06 +0200

    Spring Boot Initialization

---
+++ src/main/java/com/bosonit/springboot/bs12/Bs12Application.java
@@ -0,0 +1,13 @@
+package com.bosonit.springboot.bs12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bs12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bs12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bosonit/springboot/bs12/Bs12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.springboot.bs12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bs12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
